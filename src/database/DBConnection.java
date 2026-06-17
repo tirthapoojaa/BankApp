06aaa8ab967108e054ced99ca79bbb27140d7a8d
“tirthapoojaa” <tn0669@srmist.edu.in>
2026-06-17 11:24:28 +0530

    JDBC connection added

---
+++ src/database/DBConnection.java
@@ -0,0 +1,27 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/banking_app";
+
+    private static final String USER = "root";
+
+    private static final String PASSWORD = "root123";
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("MySQL Driver not found", e);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
---
+++ src/database/TestConnection.java
@@ -0,0 +1,21 @@
+package database;
+
+import java.sql.Connection;
+
+public class TestConnection {
+
+    public static void main(String[] args) {
+
+        try {
+            Connection con = DBConnection.getConnection();
+
+            System.out.println("Database Connected Successfully!");
+            System.out.println(con);
+
+            con.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
